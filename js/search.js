2c1af648bcbef60b869da1aa360f5edd990d67e6
HuzerOVO <HuzerOVO@users.noreply.github.com>
2023-07-14 07:18:32 +0000

    Deploy by Github Actions HuzerOVO/blog-repo@fe0a594dee7fcda4aa197dcfa2d87b48cda4c3b8

--- js/search.js
+++ js/search.js
@@ -7,81 +7,120 @@ function regex_error() {
     $('#search').attr('style', 'color: red;');
 }
 
+// 正则搜索
 function search_with_regex(k, article) {
     let regex = null;
-    let keywords = [];
+    let html_str = '';
     try {
         regex = new RegExp(k, 'g');
     } catch {
         regex_error();
+        return html_str;
     }
-    keywords = keywords.concat(article.title.match(regex));
-    keywords = keywords.concat(article.content.match(regex));
-    return search_with_keywords(keywords, article);
-}
 
-function search_with_keywords(keywords, article) {
-    let html_str = '';
-    let title = article.title;
-    let content = article.content;
-    let start = article.content.length;
-    let end = -1;
-    let ok = [];
-    let match = false;
-    let content_match = false;
-    // 查找所有关键字
-    if (article.title !== '' && article.content !== '') {
-        // 这里可能需要对'mark'这个特殊的关键字进行额外的处理
-        keywords.forEach(function(k) {
-            if (!ok.includes(k)) {
-                ok.push(k);
-                let idx_title = article.title.indexOf(k);
-                let idx_content = article.content.indexOf(k);
-                if (idx_title >= 0 || idx_content >= 0) {
-                    match = true;
-                    title = title.replace(k, '<mark>' + k + '</mark>')
-                    if (idx_content >= 0) {
-                        content_match = true;
-                        content = content.replace(k, '<mark>' + k + '</mark>');
-                        if (start > idx_content) {
-                            start = idx_content;
-                        }
-                        if (end < idx_content) {
-                            end = idx_content;
-                        }
-                    }
+    try {
+        if (article.title !== '' && article.content !== '') {
+            let title = article.title;
+            let content = article.content;
+            let match = false;
+            let start = content.length;
+            let end = -1;
+            if (title.search(regex) !== -1) {
+                match = true;
+                title = title.replace(regex, "<mark>$&</mark>");
+            }
+
+            start = content.search(regex);
+            end = start + 80;
+            if (start !== -1) {
+                match = true;
+            }
+
+            if (match) {
+                if (start <= 0) {
+                    start = 0;
+                    end = Math.min(180, content.length);
+                } else {
+                    start = Math.max(0, start - 80);
+                    end = Math.min(content.length, end);
                 }
-            }
-        });
-        if (match) {
-            if (content_match) {
-                start = Math.max(start - 60, 0);
-                end = Math.min(end + 60, content.length);
-            } else {
-                start = 0;
-                end = Math.min(120, content.length);
-            }
-            html_str = `
+                // 先替换关键字的话，可能会在<mark>或</mark>标签中断开
+                // 导致显示错误。
+                content = content.slice(start, end).replace(regex, "<mark>$&</mark>");
+                html_str = `
         <article class="articles-list-item">
             <a href="${article.url}">
-                <h3>${title}</h3>
-                <p>
-                    ${content.slice(start, end)}
+                <p class="item-title">
+                    ${title}
+                </p>
+                <p class="item-content">
+                    ${content}
+                </p>
+            </a>
+        </article>`;
+            }
+        }
+        return html_str;
+    } catch (e) {
+        console.log(e);
+    }
+
+    return html_str;
+}
+
+// 关键字搜索
+function search_with_keywords(keywords, article) {
+    let title = article.title;
+    let content = article.content;
+    let match = false;
+    if (title !== '' && content !== '') {
+        // 按照关键字顺序查找，在第一次查找成功后或者查找失败时跳出
+        for (let i in keywords) {
+            let k = keywords[i];
+            // 在输入关键字处理时，去除首尾空格可能导致空字符
+            if (k === '') {
+                continue;
+            }
+            let idx_title = title.indexOf(k);
+            let idx_content = content.indexOf(k);
+            if (idx_title >= 0 || idx_content >= 0) {
+                match = true;
+                title = title.replaceAll(k, '<mark>' + k + '</mark>');
+                if (idx_content >= 0) {
+                    let start = Math.max(0, idx_content - 80);
+                    let end = Math.min(content.length, start + 160);
+                    content = content.slice(start, end);
+                    content = content.replaceAll(k, '<mark>' + k + '</mark>');
+                }
+                break;
+            }
+        }
+
+        // 如果有匹配的
+        if (match) {
+            return `
+        <article class="articles-list-item">
+            <a href="${article.url}">
+                <p class="item-title">
+                    ${title}
+                </p>
+                <p class="item-content">
+                    ${content}
                 </p>
             </a>
         </article>`;
         }
     }
-    return html_str;
+    return '';
 }
 
 // 这个实现在具有大量文章时可能表现不佳
 function searchArticle(path) {
-    $('#search-result .articles-list').empty();
     clear_error();
-    // 空格分割字符
     let pattern = $('#search').val().trim();
+    // 输入为空
     if (pattern.length < 1) {
+        $('#search-result .articles-list').empty();
         return;
     }
 
@@ -92,7 +131,16 @@ function searchArticle(path) {
         keywords = pattern.replace(/^\//, '').replace(/\/$/, '');
         use_regex = true;
     } else {
-        keywords = pattern.split(/[\s]+/);
+        // '+'分割字符
+        keywords = pattern.split('+');
+        // 去除首尾空格
+        keywords.forEach(function(e, i, arr){
+            arr[i] = e.trim();
+        });
+        // 按照关键字长度，降序排序
+        keywords.sort(function(a, b) {
+            return b.length - a.length
+        })
         use_regex = false;
     }
 
@@ -115,6 +163,7 @@ function searchArticle(path) {
             }
 
             // 在文章的标题与内容中查找关键字
+            $('#search-result .articles-list').empty();
             articles.forEach(function(article) {
                 let html_str = '';
                 // 使用正则查找
@@ -129,7 +178,7 @@ function searchArticle(path) {
             })
         }, // success END
         error: function() {
-            console.log("Cannot request: " + path);
+            console.log("Request '" + path + "' failed in ajax.");
         },
     });
 }
